b9eb8ce4b1c41d270550ec9b2b8a61b73792ab2d
vynas <hamdabousnina@gmail.com>
2025-05-30 20:10:05 -0400

    fetch Projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,7 @@
+import { useFetchProject } from "./fetchProjects";
+
 const Projects = () => {
+  const { loading, projects } = useFetchProject();
   return <h2>Projects</h2>;
 };
 export default Projects;
